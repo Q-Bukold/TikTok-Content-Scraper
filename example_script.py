5b04c8cd741a090addd8b49a0e3b33b278015bb2
Quentin Bukold <83951996+Q-Bukold@users.noreply.github.com>
2025-02-16 21:45:17 +0100

    improved wording and formating in readme

--- example_script.py
+++ example_script.py
@@ -1,7 +1,7 @@
 from TT_Scraper import TT_Scraper
 
 # Configure the scraper, this step is always needed
-tt = TT_Scraper(wait_time=0.3, output_files_fp="data/")
+tt = TT_Scraper(wait_time=0.3, output_files_fp="data2/")
 
 # Download all metadata as a .json and all content as .mp4/.jpeg
-tt.scrape(id = 7460303767968156958, scrape_content = True)
+tt.scrape_list(ids = [7460303767968156958], scrape_content = True, clear_console=True)
