f970fe997b35e8946dec518c74088e9bc59845dc
Quentin Bukold <83951996+Q-Bukold@users.noreply.github.com>
2025-04-16 19:56:54 +0200

    added function for scraping authors and improved documentation

--- TT_Scraper/__init__.py
+++ TT_Scraper/__init__.py
@@ -23,8 +23,44 @@ class TT_Scraper(HTML_Scraper):
     from ._exception_handler import _exception_handler
 
 
-    
-    def scrape_list(self, ids : list = None, scrape_content : bool = True, batch_size : int = None, clear_console = True, total_videos=0, already_scraped_count=0, total_errors=0):
+    def scrape_list(self, ids : list = None, scrape_content : bool = True, batch_size : int = None, clear_console = False, total_videos=0, already_scraped_count=0, total_errors=0):
+        '''
+        This pipeline makes use of the scrape function to scrape a list of video or slide IDs.
+        
+        This function handles errors. If a known error is occuring, the error code will be part of the metadata package which is downloaded to the output folder as a json.
+        
+        Error Codes:
+        error_code = "D" -> "No data from URL"
+        error_code="I" -> "The scraped HTML code could not provide the searched metadata elements"
+        error_code="V" -> "No data from URL" <legacy error code>
+        error_code="O" -> "Some other error" <code is only used in development>
+
+        Parameters
+        ----------
+        id : str 
+            The id of the TikTok content is typically found in the URL when sharing a video or slide.
+
+        scrape_content : bool
+            False = The scraper only scrapes metadata.
+            True = The scraper also scrapes the content binaries.
+
+        batch_size : int
+            Configures how many data is scraped before it is writen to the output folder. A higher batch size can lead to a faster scraper, but more memory usage. This parameter is also a first step towards multithreading the writig process.
+        
+        clear_console : bool
+            !Caution this function only works on linux and mac!
+            False = Nothing happens.
+            True = The console output is cleared after every download. This improves readability.
+        
+        total_videos : int
+            This parameter changes the initial value of the logger to a specific integer. This is mainly useful, if you connect the scraper to a database.
+
+        already_scraped_count : int
+            This parameter changes the initial value of the logger to a specific integer. This is mainly useful, if you connect the scraper to a database.
+        
+        total_errors : int
+            This parameter changes the initial value of the logger to a specific integer. This is mainly useful, if you connect the scraper to a database.
+        '''
 
         # initialisation        
         self.queue_length = len(ids)
@@ -92,7 +128,34 @@ class TT_Scraper(HTML_Scraper):
 
     def scrape(self, id, scrape_content : bool = False, download_metadata = True, download_content = True):
         """
-        Scrapes a single TikTok video based on its ID.
+        Scrapes a single TikTok video based on its ID. A slide returns a json-like dictionary for all metadata and a dictionary containing the pictures and music. A video returns a json-like dictionary for all metadata and the binaries of the mp4.
+
+
+        This function handles errors. If a known error is occuring, the error code will be part of the metadata package which is ether downloaded to the output folder as a .json or returned.
+        
+        Error Codes:
+        error_code = "D" -> "No data from URL"
+        error_code="I" -> "The scraped HTML code could not provide the searched metadata elements"
+        error_code="V" -> "No data from URL" <legacy error code>
+        error_code="O" -> "Some other error" <code is only used in development>
+
+        Parameters
+        ----------
+        id : str 
+            The id of the TikTok content is typically found in the URL when sharing a video or slide.
+
+        scrape_content : bool
+            False = The scraper only scrapes metadata.
+            True = The scraper also scrapes the content binaries.
+
+        download_metadata : bool
+            True = The metadata is downloaded to the output folder specifed when initiating the TT_Scraper Class. 
+            False = The metadata is returned as an output of this function.
+        
+        download_content : bool
+            True = The content (mp4, mp3, jpeg) is downloaded into the output folder specifed when initiating the TT_Scraper Class.
+            False = The content binaries are returned as an output of this function
+
         """
         id = str(id)
         try:
@@ -115,20 +178,20 @@ class TT_Scraper(HTML_Scraper):
                 self.log.info(f"https://www.tiktok.com/@tiktok/video/{id}")
                 raise NoDataFromURL
             metadata_package = self._filter_tiktok_data(interesting_elements)
-            video_binary = None
-            slide_pictures = None
-            slide_audio = None
+            binary_video = None
+            binaries_slides = None
+            binary_slides_audio = None
+
             filepath = f"{self.VIDEOS_OUT_FP}tiktok_{id}_*"
             metadata_package["file_metadata"]["filepath"] = filepath
 
             # scraping content, if requested by user
             if scrape_content:
-
                 try:
-                    video_binary = self._scrape_video(metadata = requested_data_str)
+                    binary_video = self._scrape_video(metadata = requested_data_str)
                     metadata_package["file_metadata"]["is_slide"] = False
                 except VideoIsPicture:
-                    slide_pictures, picture_formats, slide_audio = self._scrape_picture(metadata = requested_data_str)
+                    binaries_slides, picture_formats, binary_slides_audio = self._scrape_picture(metadata = requested_data_str)
                     metadata_package["file_metadata"]["is_slide"] = True
                     metadata_package["file_metadata"]["picture_formats"] = picture_formats
 
@@ -163,12 +226,11 @@ class TT_Scraper(HTML_Scraper):
                 self.log.ERROR("too many errors in a row")
                 sys.quit(1)
         
-        
-        # create array of binary content (videos, pictures, music) 
-        if video_binary:
-            content_binary = {"type": "video", "mp4_binary": video_binary}
-        elif slide_pictures:
-            content_binary = {"type": "slide", "slide_pictures": slide_pictures, "slide_audio": slide_audio}
+        # create package of binary contents (videos, pictures, music) 
+        if binary_video:
+            content_binary = {"type": "video", "mp4_binary": binary_video}
+        elif binaries_slides:
+            content_binary = {"type": "slide", "slide_pictures": binaries_slides, "slide_audio": binary_slides_audio}
         else:
             content_binary = None      
 
@@ -187,3 +249,28 @@ class TT_Scraper(HTML_Scraper):
         else:
             return metadata_package, content_binary
 
+    def scrape_user(self, username, download_metadata = True):
+        if "@" in username:
+            username = str.replace(username, "@", "")
+        
+        # scraping html data
+        requested_data = self.request_and_retain_cookies(f"https://www.tiktok.com/@{username}")
+        soup = BeautifulSoup(requested_data.text, "html.parser")
+        rehydration_data_json = soup.find('script', attrs={'id':"__UNIVERSAL_DATA_FOR_REHYDRATION__"})
+
+        rehydration_data_json = json.loads(rehydration_data_json.string)
+
+        # filtering html data
+        with open(f"test_folder/rehydration.json", "w", encoding="utf-8") as f:
+            json.dump(rehydration_data_json, f, ensure_ascii=False, indent=4)
+
+        user_data = rehydration_data_json["__DEFAULT_SCOPE__"]["webapp.user-detail"]["userInfo"]
+
+        filepath = f"{self.VIDEOS_OUT_FP}tiktok_user_{username}.json"
+
+        if download_metadata:
+            self.write_metadata_package(filepath=filepath, metadata_package=user_data)
+        else:
+            return user_data
+
+        # handling exceptions        
