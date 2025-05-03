14bf75c38ba5b20dac928d7c1b5727e4f07430de
Quentin Bukold <83951996+Q-Bukold@users.noreply.github.com>
2025-05-03 21:51:42 +0200

    Established first version of pipeline, featuring user scraping, download and output_location.

--- TT_Scraper_II/pipeline.py
+++ TT_Scraper_II/pipeline.py
@@ -1,5 +1,26 @@
 from scrape_user import user_scraper
 from pprint import pprint
+import os
+import json
 
-json_like = user_scraper("cdu")
-pprint(json_like)
+def scrape_list(input : list, output_folder : str = "scraped_data"):
+    _init_output_location(output_folder)
+
+    for i, x in enumerate(input):
+        user_metadata = user_scraper(x)
+        write_metadata_package(filepath=f"{output_folder}/user_{x}.json", metadata_package=user_metadata)
+
+
+def _init_output_location(output_folder : str):
+    # if the directory is not present then create it. 
+    if not os.path.isdir(output_folder): 
+        os.makedirs(output_folder)
+
+def write_metadata_package(filepath, metadata_package):
+    with open(filepath, "w", encoding="utf-8") as f:
+        json.dump(metadata_package, f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    input_lst = ["cdu", "7496425065773288726"]
+
+    scrape_list(input_lst)
