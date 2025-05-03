7cc7dcc01c74d70b9be2d9cb75fa0e7eac8211ba
Quentin Bukold <83951996+Q-Bukold@users.noreply.github.com>
2025-05-03 23:17:08 +0200

    added content metadata scraping

--- TT_Scraper_II/pipeline.py
+++ TT_Scraper_II/pipeline.py
@@ -1,10 +1,13 @@
-from scrape_user import user_scraper
 from pprint import pprint
 import os
 import json
+import pandas as pd
 from prefect import flow, task
 from prefect.logging import get_run_logger
 from prefect.task_runners import SequentialTaskRunner
+
+from scrape_user import user_scraper
+from scrape_metadata import metadata_scraper
 
 @task(name="creating output folder")
 def _init_output_location(output_folder : str):
@@ -21,16 +24,22 @@ def write_metadata_package(filepath, metadata_package):
 def user_scraper_p(x):
     return user_scraper(x)
 
+@task(name="scraping user")
+def metadata_scraper_p(x):
+    return metadata_scraper(x)
+
 @flow(log_prints=True, name="TikTok-Content-Scraper", task_runner=SequentialTaskRunner())
-def scrape_list(input : list, output_folder : str = "scraped_data"):
+def scrape_list(output_folder : str = "scraped_data"):
     logger = get_run_logger()
+    input = pd.read_pickle("seedlist.pkl")
     _init_output_location(output_folder)
 
     for i, x in enumerate(input):
         x = str(x)
         if x.isdigit():
             # start content scraping
-            continue
+            content_metadata = metadata_scraper_p(x)
+            write_metadata_package(filepath=f"{output_folder}/content_{x}.json", metadata_package=content_metadata)
         else:
             # start user scraping
             user_metadata = user_scraper(x)
@@ -39,4 +48,5 @@ def scrape_list(input : list, output_folder : str = "scraped_data"):
 
 if __name__ == "__main__":
     input_lst = ["cdu", "7496425065773288726"]
-    scrape_list(input_lst)
+    pd.to_pickle(input_lst, "seedlist.pkl")
+    scrape_list()
